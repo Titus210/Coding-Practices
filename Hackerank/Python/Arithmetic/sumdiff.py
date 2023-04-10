672893dfcb491c0ea2b8ef9261fc47add2a6906d
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-11 02:46:03 +0300

    Arithmetic operations

---
+++ Hackerank/Python/Arithmetic/sumdiff.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+sumOfNo = a + b
+differenceOfNo = a - b
+productOfNo = a * b
+
+print(f'{sumOfNo}')
+print(f'{differenceOfNo}')
+print(f'{productOfNo}')
