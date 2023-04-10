d3e78cdb7cc3ec59101a5106ab0278969ac3bfbc
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-11 02:42:09 +0300

    Addd inclusive form of 20

--- Hackerank/Python/If else/ifElse.py
+++ Hackerank/Python/If else/ifElse.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 if (n % 2) == 0:
     if n in range(2,5):
         print("Not Weird")
-    if n in range(6,20):
+    if n in range(6,20 + 1):
         print("Weird")
     if n > 20:
         print("Not Weird")
