443ff6c193b6ca3250514fb2da870a794e80eb5e
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-05-07 13:13:29 +0300

    Leet code two sum number

---
+++ Leetcode/1.two-sum.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function (nums, target) {
+    for (i = 0; i < nums.length; i++) {
+        for (j = i + 1; j < nums.length; j++) {
+            if ((nums[i] + nums[j]) == target) {
+                return [i, j];
+            }
+        }
+    }
+    return null; // handle case when no matching pair is found
+};
+
+console.log(twoSum([3, 2, 4], 6)); // gives [1,2] as expected
+
+// @lc code=end
